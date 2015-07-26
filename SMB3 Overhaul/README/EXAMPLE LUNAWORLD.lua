bdcbe00059825ce6f17db765d352566fc274cb46
XNBlank <koruten@gmail.com>
2015-07-26 12:09:01 -0700

    Overhaul API Version 1.0

---
+++ SMB3 Overhaul/README/EXAMPLE LUNAWORLD.lua
@@ -0,0 +1,9 @@
+_smb3overhaul = loadAPI("smb3overhaul");
+
+function onLoad()
+	_smb3overhaul.setSecondsLeft(200);
+	_smb3overhaul.setTimerState(true);
+	_smb3overhaul.usesCard(true);
+	_smb3overhaul.usesPBar(true);
+end
+
